89b0a7e7070ad7ba84e10a17b24b4d1ede4bac31
Connor P <71726066+s1306843@users.noreply.github.com>
2021-10-12 22:27:47 -0400

    First Commit

---
+++ CS176L_Arrays/prob1/TestSumOfDice.java
@@ -0,0 +1,20 @@
+package labproject3.prob1;
+
+import java.util.Scanner;
+
+public class TestSumOfDice 
+{
+
+	public static void main(String[] args) 
+	{
+		
+		SumOfDice call = new SumOfDice();
+		
+		call.diceRolls();
+		
+		System.out.println();
+		
+		call.freqChart();
+	}
+
+}
